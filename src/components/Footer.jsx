6adf0e88bdd1d13d71c4d1d10da9e5e9dc4435ea
TravisBuilds <dev@lumierevr.com>
2021-06-06 13:03:30 -0700

    social media handle update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,11 @@ import Image from 'react-bootstrap/Image';
 import footer from '../assets-new/footer.png';
 import telegram from '../assets-new/telegram.png';
 import github from '../assets-new/github.png';
+import twitter from '../assets-new/twitter.png';
+import discord from '../assets-new/discord.png';
+import youtube from '../assets-new/youtube.png';
 import gitbook from '../assets-new/gitbook.png';
+import facebook from '../assets-new/facebook.png';
 
 function Footer() {
   return (
@@ -22,13 +26,13 @@ function Footer() {
         <Row>
           <Col></Col>
           <Col></Col>
-          <Col></Col>
-          <Col></Col>
+          <Col><a href='https://www.facebook.com/highstreetmkt'><Image style={{width: '30px'}} src={facebook} alt="facebook" fluid /></a></Col>
+          <Col><a href=' https://twitter.com/highstreetworld'><Image style={{width: '30px'}} src={twitter} alt="twitter" fluid /></a></Col>
           <Col><a href='https://t.me/highstreetmarket'><Image style={{width: '30px'}} src={telegram} alt="telegram" fluid /></a></Col>
           <Col><a href='https://github.com/TravisBuilds/HighStreet'><Image style={{width: '30px'}} src={github} alt="github" fluid /></a></Col>
           <Col><a href='https://highstreet-market.gitbook.io/highstreet/'><Image style={{width: '30px'}} src={gitbook} alt="gitbook" fluid /></a></Col>
-          <Col></Col>
-          <Col></Col>
+          <Col><a href='https://www.youtube.com/channel/UCzXzF5_1RQupiUG0pVGJZmA'><Image style={{width: '30px'}} src={youtube} alt="youtube" fluid /></a></Col>
+          <Col><a href='https://discord.gg/wxm6JNEbD9'><Image style={{width: '30px'}} src= {discord} alt ="discord" fluid/></a></Col>
           <Col></Col>
           <Col></Col>
         </Row>
